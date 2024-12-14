bfd04abb6188744c3e8605d525ab8bcd79c54397
Ulugbek Rahmonov <zzephyrrpro@gmail.com>
2024-12-15 06:52:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ if plt == 'Linux': pathlib.WindowsPath=pathlib.PosixPath
   
 st.title("Havo yo'llarida harakatlanuvchi vositalarni klassifikatsiya qiluvchi model")
 file=st.file_uploader('Rasm yuklash', type=['png','jpeg','gif','svg','jpg'])
-st.image(file)
-img=PILImage.create(file)
-model=load_learner('aerial-vehicles.pkl')
-pred, pred_id, probs=model.predict(img)
-st.success(f"Bashorat: {pred}")
-st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+if file:
+   st.image(file)
+   img=PILImage.create(file)
+   model=load_learner('aerial-vehicles.pkl')
+   pred, pred_id, probs=model.predict(img)
+   st.success(f"Bashorat: {pred}")
+   st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
