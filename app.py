1c3dfb709bb06d894cd2e66296a7ef9252e5fb94
Ulugbek Rahmonov <zzephyrrpro@gmail.com>
2024-12-15 08:28:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from fastai.vision.all import *
+import platform
+import pathlib
 from pathlib import Path
 plt=platform.system()
 if plt =='Linux': pathlib.WindowsPath=pathlib.PosixPath
