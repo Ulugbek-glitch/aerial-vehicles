9d327bf0ff43f5b8357882a3618b93a47ffa778c
Ulugbek Rahmonov <zzephyrrpro@gmail.com>
2024-12-15 08:14:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@
 #st.title("Havo yo'llarida harakatlanuvchi vositalarni klassifikatsiya qiluvchi model")
 #file=st.file_uploader('Rasm yuklash', type=['png','jpeg','gif','svg'])
 #if file:
-   st.image(file)
-   img=PILImage.create(file)
-   model=load_learner('aerial-vehicles.pkl') 
-   pred, pred_id, probs=model.predict(img)
-   st.success(f"Bashorat: {pred}")
-   st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+   #st.image(file)
+   #img=PILImage.create(file)
+   #model=load_learner('aerial-vehicles.pkl') 
+   #pred, pred_id, probs=model.predict(img)
+   #st.success(f"Bashorat: {pred}")
+   #st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
 
 import streamlit as st
 from fastai.vision.all import *
